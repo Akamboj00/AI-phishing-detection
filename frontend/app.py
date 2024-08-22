d28d1e0d5c1d7ec20b2e03a9245bd1c47fb10778
Akamboj00 <Abhilash.Kamboj@gmail.com>
2024-08-22 15:56:53 +0100

    Started implementing frontend

---
+++ frontend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/train')
+def train():
+    return render_template('train.html')
+
+@app.route('/detect')
+def detect():
+    return render_template('detect.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
